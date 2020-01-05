6a7f7f31aeb3794a834fed27e7b34d7abdae35b4
amir312 <57588920+amir312@users.noreply.github.com>
2020-01-05 17:48:15 +0200

    Add files via upload

---
+++ Controllers/OrderFlightController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HW3.Models;
+
+namespace HW3.Controllers
+{
+    public class OrderFlightController : ApiController
+    {
+       
+        public void Post([FromBody]OrderdFlight oflt1)
+        {
+            oflt1.insertOrderdFlight();
+
+
+        }
+
+     
+    }
+}
