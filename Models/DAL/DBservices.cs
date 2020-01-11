ee203e6b3e18dc51c3260b561d65ce9592ab8580
tamir-aro <57325624+tamir-aro@users.noreply.github.com>
2020-01-11 16:37:34 +0200

    Update DBservices.cs

--- Models/DAL/DBservices.cs
+++ Models/DAL/DBservices.cs
@@ -674,7 +674,43 @@ public class DBservices
         return discounts;
     }
 
-
+ //  DELETE Discount
+    //--------------------------------------------------------------------------------------------------
+    public int DeleteDiscount(int Id)
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        try
+        {
+            con = connect("DBConnectionString"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+        String cStr = "delete from Discounts where ID = " + Id.ToString();      // helper method to build the insert string
+        cmd = CreateCommand(cStr, con);             // create the command
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery(); // execute the command
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            return 0;
+            // write to log
+            throw (ex);
+        }
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+    }
 
 
 }
