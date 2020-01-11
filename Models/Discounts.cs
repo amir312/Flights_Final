53841a2255727adbc586eeda3633f86b1ec35f03
tamir-aro <57325624+tamir-aro@users.noreply.github.com>
2020-01-11 16:53:39 +0200

    Add files via upload

--- Models/Discounts.cs
+++ Models/Discounts.cs
@@ -12,10 +12,11 @@ namespace HW3.Models
         string flyto;
         string startdate;
         string finishdate;
-        float discount;
+        decimal discount;
+        int id;
 
         public Discounts() { }
-        public Discounts(string airline, string flyfrom, string flyto, string startdate, string finishdate, float discount)
+        public Discounts(string airline, string flyfrom, string flyto, string startdate, string finishdate, decimal discount, int id)
         {
             this.airline = airline;
             this.flyfrom = flyfrom;
@@ -23,6 +24,7 @@ namespace HW3.Models
             this.startdate = startdate;
             this.finishdate = finishdate;
             this.discount = discount;
+            this.id = id;
         }
 
         public string Airline { get => airline; set => airline = value; }
@@ -30,14 +32,21 @@ namespace HW3.Models
         public string Flyto { get => flyto; set => flyto = value; }
         public string Startdate { get => startdate; set => startdate = value; }
         public string Finishdate { get => finishdate; set => finishdate = value; }
-        public float Discount { get => discount; set => discount = value; }
+        public decimal Discount { get => discount; set => discount = value; }
+        public int Id { get => id; set => id = value; }
 
-        public List<Discounts> getusers()
+        public List<Discounts> getdiscounts()
         {
             List<Discounts> u = new List<Discounts>();
             DBservices dbs = new DBservices();
             u = dbs.getdiscounts();
             return u;
+        }
+
+        public void DeleteDiscount (int id)
+        {
+            DBservices dbs = new DBservices();
+            dbs.DeleteDiscount(id);
         }
     }
 }
